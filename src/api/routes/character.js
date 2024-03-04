c40d274e634a480e3a742d0f5ce6c747fcb860a0
JackPliskin64 <jackpliskin64@outlook.es>
2024-03-04 16:34:23 +0100

    updated

--- src/api/routes/character.js
+++ src/api/routes/character.js
@@ -6,11 +6,13 @@ const {
   getCharactersByAge
 } = require('../controllers/character')
 const characterRoutes = require('express').Router()
+const { isAdmin } = require('../../middlewares/auth')
+const { getUsers, register, login } = require('../controllers/users')
 
 characterRoutes.get('/', getCharacters)
 characterRoutes.get('/findByAge/:age', getCharactersByAge)
 characterRoutes.post('/', postCharacter)
 characterRoutes.put('/:id', updateCharacter)
-characterRoutes.delete('/:id', deleteCharacter)
+characterRoutes.delete('/:id', [isAdmin], deleteCharacter)
 
 module.exports = characterRoutes
