0d24da0a6348fbe01ce6ab695efdf76d7f84aa5b
JackPliskin64 <jackpliskin64@outlook.es>
2024-04-22 17:49:18 +0200

    Fixed some changes

--- src/api/routes/character.js
+++ src/api/routes/character.js
@@ -6,13 +6,12 @@ const {
   getCharactersByAge
 } = require('../controllers/character')
 const characterRoutes = require('express').Router()
-const { isAdmin } = require('../../middlewares/auth')
-const { getUsers, register, login } = require('../controllers/users')
+const { isAuth, isAdmin } = require('../../middlewares/auth')
 
 characterRoutes.get('/', getCharacters)
 characterRoutes.get('/findByAge/:age', getCharactersByAge)
-characterRoutes.post('/', postCharacter)
-characterRoutes.put('/:id', updateCharacter)
+characterRoutes.post('/', [isAuth], postCharacter)
+characterRoutes.put('/:id', [isAuth], updateCharacter)
 characterRoutes.delete('/:id', [isAdmin], deleteCharacter)
 
 module.exports = characterRoutes
